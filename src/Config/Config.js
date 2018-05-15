319363d0ea1dbc2f9c6b75745f15fe8a5793dbcb
Ahmet <aahmet.272@gmail.com>
2018-05-15 11:31:50 +0300

    Update Config.js

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,8 +1,8 @@
 export const DB_CONFIG = {
-  apiKey: "AIzaSyD0Alv6heUUXaW6KnDdYI7YfkokYUKrajA",
-  authDomain: "to-do-b3b6f.firebaseapp.com",
-  databaseURL: "https://to-do-b3b6f.firebaseio.com",
-  projectId: "to-do-b3b6f",
-  storageBucket: "to-do-b3b6f.appspot.com",
-  messagingSenderId: "1030231749657"
+  apiKey: "***********************************",
+  authDomain: "***********************************",
+  databaseURL: "***********************************",
+  projectId: "******************",
+  storageBucket: "********************",
+  messagingSenderId: "*******************"
 };
